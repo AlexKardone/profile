0ef7a2f436dd7202f4d280eb1dafa4e060c3a5e7
Alexander Matskevich <alex-kardone@yandex.ru>
2017-05-31 10:53:28 +0300

    Первый коммит, после которого будет новая ветка, где я переделаю весь middle-block

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Matskevich</title>
+	<link rel="stylesheet" type="text/css" href="styles.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="format-detection" content="telephone=no">
+</head>
+<body>
+
+	<?php include("header.html") ?>
+	<?php include("blocks/content-top.html") ?>
+	<?php include("blocks/content-middle.html") ?>
+	
+<script src="script.js" type="text/javascript"></script>
+</body>
+</html>
---
+++ script.js
@@ -0,0 +1,51 @@
+var rect = document.getElementsByTagName('rect');
+console.log(rect);
+
+var menu = document.getElementsByClassName('main-menu__second-lvl');
+console.log(menu);
+
+for (var i = 0; i < rect.length; i++) {
+	rect[i].onclick = f1;
+}
+
+function f1() {
+	var el = this.nextElementSibling.classList;
+	if (el.contains('disclosure-list__arrow')) {
+		el.remove('disclosure-list__arrow');
+		el.add('disclosure-list__arrow-out');
+		menu[0].style.display = 'none';
+	} else if (el.contains('disclosure-list__arrow-out')) {
+		el.remove('disclosure-list__arrow-out');
+		el.add('disclosure-list__arrow');
+		menu[0].style.display = 'block';
+	} else {
+		el.add('disclosure-list__arrow');
+		menu[0].style.display = 'block';
+	}
+}
+
+var mobMenu = document.getElementById('menu-icon-id');
+mobMenu.onclick = f2;
+
+var mobMenuClose = document.getElementById('main-menu_mob-cross-id');
+console.log(this);
+mobMenuClose.onclick = f3;
+
+function f2() {
+	var a = document.getElementById('main-menu_mob-id');
+
+	a.style.display = 'block';
+}
+
+function f3() {
+	var a = document.getElementById('main-menu_mob-id');
+	console.log(a);
+	a.style.display = 'none';
+}
+
+document.getElementById('content-middle__video-id').onclick = hidePoster;
+
+function hidePoster() {
+	var a = document.getElementById('content-middle__video-id');
+	a.style.display = "none";
+}
